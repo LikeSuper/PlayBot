39daca8879c960af65bb871e1c790de24cbda35f
LikeSuper <dmitriirackin888@yandex.ru>
2020-10-10 11:26:18 +0400

    Upgrade Token

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'NzYyMzE3Nzc1NTg2MzI4NTk3.X3nZwA.rbDlDaz7jHN1kVOzCJqqViECZfU',
+    'token': 'TOKEN',
     'bot': 'PlayBot',
     'id': 762317775586328597,
     'prefix': '.'
